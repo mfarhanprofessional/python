00b2d844c86c3e6d6901207815ffce5214749ef3
mfarhanprofessional <mfarhanprofessional@gmail.com>
2024-11-05 01:50:56 +0500

    step3 controll flow and looping

--- app.py
+++ app.py
@@ -18,3 +18,17 @@ for x,num in enumerate(num_list):
 
 print(count)
 
+# step3
+
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+
+count = 0
+
+for x,num in enumerate(num_list):
+    count += 1
+    if num == 36:
+        print('Number found at ', x)
+        break
+
+print(count)
+
